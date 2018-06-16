d86dc19981bc084cf359dc0d81bb233ed7d3e081
coldEr66 <seal1000402@gmail.com>
2018-06-16 19:43:52 +0800

    check_game_over, game_loop, keymap

--- game_2048/main.cpp
+++ game_2048/main.cpp
@@ -7,22 +7,36 @@ private:
   int cell[4][4] = {};
   int score = 0;
   int best = 0;
+  map<char,int> keymap;
 
   void print_game_over(){ //print game over hint
+    cout<<"GAME OVER!"<<endl;
+  }
 
+  void generate_keymap(){
+    string keystr = "wdsa";
+    for(int i=0;i<4;i++) keymap[keystr[i]] = i;
   }
 
 public:
 
   void game_loop(){
-
+    char c;
+    while(cin>>c){
+      move_cells(keymap[c]);
+      if(check_game_over()){
+        print_game_over();
+        if(ask_restart()) start_new_game();
+        else break;
+      }
+    }
   }
 
   void start_new_game(){
 
   }
 
-  void ask_restart(){
+  bool ask_restart(){
 
   }
 
@@ -31,7 +45,22 @@ public:
   }
 
   bool check_game_over(){ //return if the game's over
-
+    for(int i=0;i<4;i++){
+      for(int j=0;j<4;j++){
+        if(cell[i][j]==0) return false;
+      }
+    }
+    for(int i=0;i<4;i++){
+      for(int j=0;j<3;j++){
+        if(cell[i][j]==cell[i][j+1]) return false;
+      }
+    }
+    for(int i=0;i<4;i++){
+      for(int j=0;j<3;j++){
+        if(cell[j][i]==cell[j+1][i]) return false;
+      }
+    }
+    return true;
   }
 
   void move_cells(int direction){ //move cells {0,1,2,3} = {up,right,down,left}
