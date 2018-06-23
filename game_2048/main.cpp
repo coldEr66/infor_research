ae82452b99f7c004ce1b666118b59c51a6dea224
dl8sd11 <tmd910607@gmail.com>
2018-06-23 22:25:45 +0800

    ask auto play mode

--- game_2048/main.cpp
+++ game_2048/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
   int game_mode;
   cout<<"Welcome to 2048 project!!! Please chose a mode to play..."<<endl;
-  cout<<"(0:DIY/1:AI)"<<endl;
+  cout<<"(0:DIY/1:Auto Play)"<<endl;
   cin>>game_mode;
   bool mode_chosed = false;
   ui_game root;
@@ -17,7 +17,13 @@ int main(){
       root.start_new_ui_game();
     } else if(game_mode == 1) {
       mode_chosed = true;
-      ai ai_player(0);
+      int auto_function = -1;
+      while(!(auto_function >= 0 && auto_function < 3)){
+        cout<<"Which auto play function do you want to try?"<<endl;
+        cout<<"(0:greedy 1:randomly 2:cycle)"<<endl;
+        cin>>auto_function;
+      }
+      ai ai_player(auto_function);
       ai_player.auto_play(root);
     } else {
 
