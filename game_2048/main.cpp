0d201e42f53963b44383594e8c982b29586caeb9
dl8sd11 <tmd910607@gmail.com>
2018-06-23 21:50:24 +0800

    ai choice

--- game_2048/main.cpp
+++ game_2048/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include "game.h"
 #include "ui_game.h"
+#include "ai.h"
 using namespace std;
 
 int main(){
@@ -9,13 +10,15 @@ int main(){
   cout<<"(0:DIY/1:AI)"<<endl;
   cin>>game_mode;
   bool mode_chosed = false;
+  ui_game root;
   while(!mode_chosed){
     if(game_mode == 0) {
       mode_chosed = true;
-      ui_game root;
       root.start_new_ui_game();
     } else if(game_mode == 1) {
-
+      game_mode = true;
+      ai ai_player(0);
+      ai_player.auto_play(root);
     } else {
 
     }
