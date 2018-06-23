306c118442609cf930550330d1dce90fe54478cb
dl8sd11 <tmd910607@gmail.com>
2018-06-23 12:49:36 +0800

    ask mode

--- game_2048/main.cpp
+++ game_2048/main.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 #include "game.h"
+#include "ui_game.h"
 using namespace std;
 
 int main(){
-  game root;
-  root.start_new_game();
+  int game_mode;
+  cout<<"Welcome to 2048 project!!! Please chose a mode to play..."<<endl;
+  cout<<"(0:DIY/1:AI)"<<endl;
+  cin>>game_mode;
+  bool mode_chosed = false;
+  while(!mode_chosed){
+    if(game_mode == 0) {
+      mode_chosed = true;
+      ui_game root;
+      root.start_new_ui_game();
+    } else if(game_mode == 1) {
+
+    } else {
+
+    }
+
+  }
   return 0;
 }
