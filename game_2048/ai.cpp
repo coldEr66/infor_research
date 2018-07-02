7eeda6f7325186289603bb86cfb54a49727aded3
dl8sd11 <tmd910607@gmail.com>
2018-07-02 21:13:03 +0800

    TD0 learning

--- game_2048/ai.cpp
+++ game_2048/ai.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
+#include <cstring>
 #include "ai.h"
 #include "game.h"
 using namespace std;
+
 ai::ai(){
   ai_mode = 0;
 }
@@ -20,6 +23,8 @@ void ai::auto_play(ui_game &board){
     play_randomly(board);
   } else if(ai_mode == 2){
     play_cycle(board);
+  } else if(ai_mode == 3) {
+    play_td0(board);
   }
 }
 
@@ -51,4 +56,76 @@ void ai::play_greedy(ui_game &board){
     board.play_by_direction(best_choice);
   }
 
+}
+void td_state::set_game_state(state input_state){
+  game_state = input_state;
+}
+void td_state::set_gained_score(int score){
+  gained_score = score;
+}
+void td_state::set_chosed_direction(int direction){
+  chosed_direction = direction;
+}
+state td_state::get_game_state(){
+  return game_state;
+}
+int td_state::get_gained_score(){
+  return gained_score;
+}
+int td_state::get_chosed_direction(){
+  return chosed_direction;
+}
+
+void state_logger::clear_states(){
+  const int logger_size = 625;
+  for(int i=0;i<4;i++){
+    for(int j=0;j<logger_size;j++){
+      for(int k=0;k<logger_size;k++){
+        for(int l=0;l<logger_size;l++){
+          for(int m=0;m<logger_size;m++){
+            states[i][j][k][l][m] = 0;
+          }
+        }
+      }
+    }
+  }
+}
+
+void state_logger::update_state(state input_state,int direction,int scored,double learning_rate){
+  vector<int> hashed_state = input_state.get_hash();
+  int &cur_state = states[direction][hashed_state[0]][hashed_state[1]][hashed_state[2]][hashed_state[3]];
+  cur_state += (scored-cur_state) * learning_rate;
+}
+
+int state_logger::best_direction(state current_state){
+  vector<int> hashed_state = current_state.get_hash();
+  int ret = rand()%4;
+
+  for(int i=0;i<4;i++){
+    if(states[i][hashed_state[0]][hashed_state[1]][hashed_state[2]][hashed_state[3]] > states[ret][hashed_state[0]][hashed_state[1]][hashed_state[2]][hashed_state[3]])ret = i;
+  }
+
+  return ret;
+}
+void ai::play_td0(ui_game &board){
+  state_logger logger;
+  for(int i=0;i<1000;i++){
+    vector<td_state> training_data;
+    while(!board.check_game_over()){
+      vector<int> now = board.get_board();
+      td_state now_td_state;
+      state now_state(now);
+      fake_game.set_board(now);
+      int best_choice = logger.best_direction(now);
+      now_td_state.set_game_state(now_state);
+      now_td_state.set_gained_score(fake_game.move_cells(best_choice));
+      now_td_state.set_chosed_direction(best_choice);
+      board.play_by_direction(best_choice);
+    }
+    int score_total = 0;
+    for(auto it=training_data.rbegin();it!=training_data.rend();it++){
+      score_total += (*it).get_gained_score();
+      logger.update_state((*it).get_game_state(),(*it).get_chosed_direction(),score_total,0.2);
+    }
+  }
 }
