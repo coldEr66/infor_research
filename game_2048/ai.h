f1b6dac665862a3fefbeb773d1d0218a0aaf5d6d
dl8sd11 <tmd910607@gmail.com>
2018-07-02 19:20:47 +0800

    define state class

--- game_2048/ai.h
+++ game_2048/ai.h
@@ -1,6 +1,28 @@
 #pragma once
 #include "game.h"
 #include "ui_game.h"
+class state{
+private:
+  vector<int> row;
+public:
+  vector<int> get_vector();
+  vector<int> get_hash();
+  void set_hash(const vector<int> &v);
+};
+class td_state{
+private:
+  state game_state;
+  int chosed_direction;
+  int gained_score;
+public:
+  void set_game_state(state input_state);
+  void set_gained_score(int score);
+  void set_chosed_direction(int direction);
+  state get_game_state();
+  int get_chosed_direction;
+  int get_gained_score;
+
+};
 class ai{
 private:
   int ai_mode;
